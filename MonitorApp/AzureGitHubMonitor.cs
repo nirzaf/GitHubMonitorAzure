b95cf04701609ecad556d08ee7ef666e52fb1d42
M F M Fazrin <mfmfazrin1986@gmail.com>
2021-09-11 02:36:12 +0530

    var queryDictionary =     Microsoft.AspNetCore.WebUtilities.QueryHelpers.ParseQuery(uri.Query);

--- MonitorApp/AzureGitHubMonitor.cs
+++ MonitorApp/AzureGitHubMonitor.cs
@@ -1,35 +1,27 @@
-using System.IO;
 using System.Net;
-using System.Threading.Tasks;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
 
 namespace MonitorApp
 {
     public static class AzureGitHubMonitor
     {
         [Function("AzureGitHubMonitor")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]
+        public static HttpResponseData Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post")]
             HttpRequestData req,
-            ILogger log)
+            FunctionContext executionContext)
         {
-            log.LogInformation("Our GitHub monitor processed an action");
+            var logger = executionContext.GetLogger("Function1");
+            logger.LogInformation("C# HTTP trigger function processed a request.");
 
-            var name = await req.ReadAsStringAsync();
-            var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
 
-            if (data != null) name ??= data.name;
+            response.WriteString($"Welcome to Azure Functions! {response.Headers}");
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name");
+            return response;
         }
     }
 }
